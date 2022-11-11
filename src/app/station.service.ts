6907ac1c66b90059dd6855b9dabca88aad79bcaa
Hussein-Elshinnawy <clorkthefirst@gmail.com>
2022-11-11 18:22:43 +0200

    add station serive getall update delete

--- src/app/station.service.ts
+++ src/app/station.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class StationService {
 
-  constructor() { }
+  constructor(private _HttpClient:HttpClient) { }
+
+  create(stationData:any):Observable<any>
+  {
+    return this._HttpClient.post('http://localhost:8080/station/create',stationData)
+
+  }
+
+  getAllStations():Observable<any>
+  {
+    return this._HttpClient.get('http://localhost:8080/station/getall')
+
+  }
+
+  deleteStation(id:number):Observable<any>
+  {
+    return this._HttpClient.delete('http://localhost:8080/station/delete/'+id)
+
+  }
+  
 }
