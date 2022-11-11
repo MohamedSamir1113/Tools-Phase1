1b5449925d6a0b52c1efa9b6fc2f34d99f5fe928
MahmoudBadr205 <93834665+MahmoudBadr205@users.noreply.github.com>
2022-11-11 17:48:14 +0200

    added register.ts

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl , FormGroup,Validators} from '@angular/forms'
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
 
 @Component({
   selector: 'app-register',
@@ -7,8 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _AuthService:AuthService , private _Router:Router) { }
 
+
+  registerForm:FormGroup = new FormGroup(
+    {
+      username:new FormControl(null,[Validators.minLength(3),Validators.required]),
+      password: new FormControl(null,[Validators.required])
+    }
+  )
+
+  submitSignUp(registerForm:FormGroup)
+  {
+    if(registerForm.valid)
+    {
+      this._AuthService.signup(registerForm.value).subscribe(
+         {
+           next:(response)=>{
+            console.log(response)
+            this._Router.navigate(['/trip'])
+          },
+           error:()=>console.log("error"),
+           complete:()=>console.log("done")
+           }
+      )
+    }
+    
+  }
+
+  
+
+  
   ngOnInit(): void {
   }
 
